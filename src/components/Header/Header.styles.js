4f2234f9804af168b44e2f2d551e163e3b2809ee
sundayonah <sundayonah94@gmail.com>
2023-09-14 17:50:24 +0100

    fix: logo

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -1,31 +1,46 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  background: var(--darkGrey);
-  padding: 0 20px;
+   background: var(--darkGrey);
+   padding: 0 20px;
 `;
 
 export const Content = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  max-width: var(--maxWidth);
-  padding: 20px 0;
-  margin: 0 auto;
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
+   max-width: var(--maxWidth);
+   padding: 20px 0;
+   margin: 0 auto;
+`;
+
+export const MyAvatar = styled.div`
+   max-width: var(--maxWidth);
+   padding: 10px 0;
+   margin: 0 auto;
+
+   h2 {
+      /* font-size: var(--fontSuperBig); */
+      color: var(--white);
+
+      @media screen and (max-width: 720px) {
+         font-size: var(--fontBig);
+      }
+   }
 `;
 
 export const LogoImg = styled.img`
-  width: 200px;
+   width: 200px;
 
-  @media screen and (max-width: 500px) {
-    width: 150px;
-  }
+   @media screen and (max-width: 500px) {
+      width: 150px;
+   }
 `;
 
 export const TMDBLogoImg = styled.img`
-  width: 100px;
+   width: 100px;
 
-  @media screen and (max-width: 500px) {
-    width: 80px;
-  }
+   @media screen and (max-width: 500px) {
+      width: 80px;
+   }
 `;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import RMDBLogo from "../../images/react-movie-logo.svg";
-import TMDBLogo from "../../images/tmdb_logo.svg";
+import RMDBLogo from '../../images/react-movie-logo.svg';
+import TMDBLogo from '../../images/tmdb_logo.svg';
 
-import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
+import {
+   Wrapper,
+   Content,
+   LogoImg,
+   TMDBLogoImg,
+   MyAvatar,
+} from './Header.styles';
 
 const Header = () => (
-  <Wrapper>
-    <Content>
-      <Link to="/">
-        <LogoImg src={RMDBLogo} alt="rmdb-logo" />
-      </Link>
-      <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
-    </Content>
-  </Wrapper>
+   <Wrapper>
+      <Content>
+         <Link to="/">
+            {/* <LogoImg src={RMDBLogo} alt="rmdb-logo" /> */}
+            <MyAvatar>
+               <h2>xhunTeq Movie</h2>
+            </MyAvatar>
+         </Link>
+         {/* <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" /> */}
+      </Content>
+   </Wrapper>
 );
 
 export default Header;
